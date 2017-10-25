ae23b438bab98624004914040c803a3472a67ce9
jiqing9006 <jiqing9006@126.com>
2017-10-25 11:17:34 +0800

    添加Test类

---
+++ application/admin/controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *
+ * @file   Index.php
+ * @date   2016-8-23 16:03:10
+ * @author Zhenxun Du<5552123@qq.com>
+ * @version    SVN:$Id:$
+ */
+
+namespace app\admin\controller;
+
+class TestController extends CommonController{
+    /**
+     * 测试页面
+     */
+    public function test(){
+
+        return "test page";
+    }
+
+
+}
